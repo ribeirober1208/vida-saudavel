f7dfbef0bb1f532073ba3d3ebddbc3a80e9d5262
anabfer <abf.ferreirac@gmail.com>
2023-09-05 18:03:17 -0300

    interface inicial tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,22 @@
 export default () => {
-    const container = document.createElement("div");
-    const template = `
-       <h1> Login </h1>
-       <a href="./#registro">Sing Up</a>
+  const container = document.createElement("div");
+  container.classList.add("formContent");
+  const template = `
+    <figure>
+      <img src="./img/Logo.png" alt="logo" class="logo">
+      <h3>Compartilhe o melhor da alimentação saudável na sua nova rede social</h3>
+    </figure>
+    <section>
+      <input type="email" name="email" placeholder="E-mail">
+      <input type="password" name="senha" placeholder="Senha">
+      <button>Entrar</button>
+      <p>ou</p>
+      <button><img src="./img/icon-google.png" alt="icon-google"><a href="">Continue com o Google</a></button>
+      <a href="./#registro">Não tem uma conta? Cadastre-se</a>    
+      </section>
+     
        `;
-    container.innerHTML = template;
-    return container;
-  };
+  container.innerHTML = template;
+  
+  return container;
+};
