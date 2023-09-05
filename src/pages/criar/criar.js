0262089f6efc4326b1331b9ec4d3176867d78958
irixalmeida <irinhaalmeida222@gmail.com>
2023-09-04 22:08:04 -0300

    adiciona item html

---
+++ src/pages/criar/criar.js
@@ -0,0 +1,8 @@
+export default () => {
+  const container = document.createElement("div");
+  const template = `
+     <h1> Criar </h1>
+     `;
+  container.innerHTML = template;
+  return container;
+};
