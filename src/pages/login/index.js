bb19bad437ea42b4cc182d373fbe34afa344cb9d
anabfer <abf.ferreirac@gmail.com>
2023-09-06 18:18:03 -0300

    estilização da tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,20 +1,28 @@
 export default () => {
   const container = document.createElement("div");
-  container.classList.add("formContent");
+  container.classList.add("container-login")
   const template = `
-    <figure>
+    <figure class="figure-login">
       <img src="./img/Logo.png" alt="logo" class="logo">
-      <h3>Compartilhe o melhor da alimentação saudável na sua nova rede social</h3>
+      <h3 class="frase-login">Compartilhe o melhor da alimentação saudável na sua nova rede social</h3>
     </figure>
-    <section>
-      <input type="email" name="email" placeholder="E-mail">
-      <input type="password" name="senha" placeholder="Senha">
-      <button>Entrar</button>
-      <p>ou</p>
-      <button><img src="./img/icon-google.png" alt="icon-google" class="icon-google"><a href="">Continue com o Google</a></button>
-      <a href="./#registro">Não tem uma conta? Cadastre-se</a>    
+    <section class="forms-login">
+    <div class="input-container">
+    <img src="./img/MaleUser.png" alt="maleUser" class="input-icon">
+    <input type="email" name="email" placeholder="E-mail" class="input-login">
+    </div>
+    <div class="input-container">
+    <img src="./img/lock.png" alt="lock" class="input-icon">
+    <input type="password" name="senha" placeholder="Senha" class="input-login">
+    <img src="./img/Hide.png" alt="hide" class="input-icon-hide">
+    </div>
+      
+      
+      <button class="button-login">Entrar</button>
+      <p class="p-login">ou</p>
+      <button class="button-google-login"><img src="./img/icon-google.png" alt="icon-google" class="icon-google"><a href="" class="continue-login">Continue com o Google</a></button>
+      <a href="./#registro" class="cadastrese-login">Não tem uma conta? Cadastre-se</a>    
       </section>
-     
        `;
   container.innerHTML = template;
   
